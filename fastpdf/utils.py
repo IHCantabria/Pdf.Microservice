ac359b67617523e91bf202d5fd8ac49132ea3357
Felipe Maza <felipe@felipem.com>
2023-01-13 13:29:31 +0100

    convert datetime

--- fastpdf/utils.py
+++ fastpdf/utils.py
@@ -1,3 +1,4 @@
+import datetime
 import logging
 import logging.config
 from pathlib import Path
@@ -8,3 +9,13 @@ def get_logger(name: str) -> object:
     logging.config.fileConfig(config_ini)
     logger = logging.getLogger(name)
     return logger
+
+
+def datetime_to_string(date_obj: datetime.datetime, format="%Y-%m-%dT%H:%M:%SZ") -> str:
+    return date_obj.strftime(format)
+
+
+def string_to_datetime(
+    str_date: str, format: str = "%Y-%m-%dT%H:%M:%SZ"
+) -> datetime.datetime:
+    return datetime.datetime.strptime(str_date, format)
