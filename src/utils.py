2005304c0b4e79c7e97ed61cf215918a1ef1bfe3
Felipe Maza <felipe@felipem.com>
2023-02-09 09:50:49 +0100

    reemplaza ruta con la del fichero .env

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,30 @@
 import datetime
 import logging
 import logging.config
+import os
 from pathlib import Path
 
 
 def get_logger(name: str) -> object:
     config_ini = Path(__file__).resolve().parent.parent / "logging.ini"
+
+    fileh = logging.FileHandler(
+        Path(os.getenv("LOG_DIR", "/app/logs/pdf"), "pdf.log"), "a"
+    )
+
     logging.config.fileConfig(config_ini)
+
+    # Cambiamos el path del fichero de log
+    LOG_PATH = Path(os.getenv("LOG_DIR", "/app/logs/pdf"))
+    if not LOG_PATH.exists():
+        LOG_PATH.mkdir(parents=True)
+
     logger = logging.getLogger(name)
+    file_handler = logger.parent.handlers[1]
+    file_handler.close()
+    file_handler.baseFilename = Path(LOG_PATH, "pdf.log")
+    file_handler.flush()
+
     return logger
 
 
